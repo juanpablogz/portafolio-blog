c8629e3b706974c60f9b0c78fd5648e7e7b7b542
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-07-11 13:01:44 -0500

    name

--- components/Experience.vue
+++ components/Experience.vue
@@ -3,7 +3,7 @@
     <h2>Sobre mi</h2>
     <p>Hola mi nombre es juan pablo, soy desarrollador de software, vivo en medellin,colombia.Desarrollo aplicaciones web, principalmente con Vuejs, Quasar y NuxtJs. En el backend he realizado Api rest con ruby on rails</p>
     <h2>Experiencia laboral</h2>
-    <div>Experiencia <strong>junio de 2018 - presente (emprendimiento propio)</strong></div>
+    <div>Especialista en casa <strong>junio de 2018 - presente (emprendimiento propio)</strong></div>
     <p>Desarrollo front end de todo el software con el que se controlan servicios médicos, especialmente servicios de fisioterapia. Este servicio puede ser tomado de manera remota y en domicilio del paciente</p>
     <div>31Rooms <strong>noviembre de 2020 - junio de 2020</strong></div>
     <p>Desarrollador front end vuejs, desarrollo de diversos proyectos con tecnologias como vuejs, nuxt y quasar. Prouectos como estrella restaurante, since.love</p>
